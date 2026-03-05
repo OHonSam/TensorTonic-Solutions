6a127ae87d632980eec11bdadecff71a0ec3e427
Ô Hớn Sâm <119187677+OHonSam@users.noreply.github.com>
2026-03-05 17:47:24 +0700

    Update solution for percentiles

--- percentiles/percentiles.py
+++ percentiles/percentiles.py
@@ -6,20 +6,19 @@ def percentiles(x, q):
     """
     # Write code here
     x = np.sort(x)
-    res = np.zeros_like(q, dtype=np.float64)
-    q_index_down = np.array(q) * (len(x) - 1) // 100
-    q_index_up = (np.array(q) * (len(x) - 1) + 99) // 100 
-    q_index = (np.array(q) * 1.0 * (len(x) - 1)) / 100
+    q_np_array = np.array(q)
+    q_index_down = q_np_array * (len(x) - 1) // 100
+    q_index_up = (q_np_array * (len(x) - 1) + 99) // 100 
+    q_index = (q_np_array * 1.0 * (len(x) - 1)) / 100
 
-    print(q_index_down)
-    print(q_index)
-    print(q_index_up)
-    
-    for i in range(len(q)):
-        idx_down = q_index_down[i] 
-        idx_up = q_index_up[i]
-        weight = q_index[i] - idx_down
-        res[i] = x[idx_down] + (weight) * (x[idx_up] - x[idx_down])
+    res = x[q_index_down] + (q_index - q_index_down) * (x[q_index_up] - x[q_index_down])
 
     return res
+    # for i in range(len(q)):
+    #     idx_down = q_index_down[i] 
+    #     idx_up = q_index_up[i]
+    #     weight = q_index[i] - idx_down
+    #     res[i] = x[idx_down] + (weight) * (x[idx_up] - x[idx_down])
+
+    # return res
     

11cb0be8714c16e04f28672c128db85bf3fd4814
Ô Hớn Sâm <119187677+OHonSam@users.noreply.github.com>
2026-03-15 21:19:18 +0700

    Add solution for rnn-cell

---
+++ rnn/rnn-cell/rnn-cell.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def tanh(x):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+def rnn_cell(x_t: np.ndarray, h_prev: np.ndarray, 
+             W_xh: np.ndarray, W_hh: np.ndarray, b_h: np.ndarray) -> np.ndarray:
+    """
+    Single RNN cell forward pass.
+    """
+    h_t = tanh(h_prev @ W_hh.T + x_t @ W_xh.T + b_h)
+    return h_t
+
+    

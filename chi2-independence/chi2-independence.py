1b7c25059e2a9b58ab1db9e4a264ce75b65ad302
Ô Hớn Sâm <119187677+OHonSam@users.noreply.github.com>
2026-03-18 21:17:31 +0700

    Update solution for chi2-independence

--- chi2-independence/chi2-independence.py
+++ chi2-independence/chi2-independence.py
@@ -8,19 +8,10 @@ def chi2_independence(C):
     total_rows_count = np.sum(C, axis=0)
     total_cols_count = np.sum(C, axis=1)
 
-    print(total_rows_count)
-    print(total_cols_count.reshape(-1, 1))
-
     grand_total_count = np.sum(total_rows_count)
-
-    print(grand_total_count)
 
     expected_frequencies = total_rows_count * total_cols_count.reshape(-1, 1) / grand_total_count
 
-    print(expected_frequencies)
-    
     chi2 = np.sum((C - expected_frequencies)**2 / expected_frequencies)
-
-    print(chi2)
 
     return chi2, expected_frequencies
